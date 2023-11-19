7e69cc4c4d0f40b511f938bb10203e616c5671bd
Travis Umbel <Umbelth94@gmail.com>
2023-11-19 11:11:16 -0600

    Added highlighting for randomized topic

--- assets/script.js
+++ assets/script.js
@@ -10,18 +10,26 @@ function listTopics() {
 function selectTopic() {
  if (randomTopic === 'HTML') {
    console.log("Let's study HTML!");
+   let element = document.getElementById('html-section');
+   element.classList.add('selected');
  } else if (randomTopic === 'CSS') {
    console.log("Let's study CSS!");
+   let element = document.getElementById('css-section');
+   element.classList.add('selected');
  } else if (randomTopic === 'Git') {
    console.log("Let's study Git!");
+   let element = document.getElementById('git-section');
+   element.classList.add('selected');
  } else if (randomTopic === 'JavaScript') {
    console.log("Let's study JavaScript!");
+   let element = document.getElementById('javascript-section');
+   element.classList.add('selected');
  } else {
    console.log('Please try again!');
  }
 }
 
-console.log('Here are the topics we learned through Prework:');
-listTopics();
-console.log('Which topic should we study first?');
-selectTopic();
+// console.log('Here are the topics we learned through Prework:');
+// listTopics();
+// console.log('Which topic should we study first?');
+// selectTopic();
