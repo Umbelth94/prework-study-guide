b2c03a5f64acc86315bb98a085e0bb2b65f6e636
Travis Umbel <Umbelth94@gmail.com>
2023-11-19 11:59:12 -0600

    topic select button functionality

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,31 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+var randomButton = document.getElementById('random-button');
+
+randomButton.addEventListener('click', () => {
+  randomize();
+  removeSelector();
+  selectTopic();
+});
 
 function listTopics() {
  for (var x = 0; x < topics.length; x++) {
    console.log(topics[x]);
  }
 }
+
+function randomize() {
+  randomTopic = topics[Math.floor(Math.random() * topics.length)];
+}
+
+function removeSelector(){
+  var selectedSection = document.querySelectorAll('section');
+  selectedSection.forEach((e) => {
+    if (e.className == 'card selected'){
+      e.classList.remove('selected');
+    }
+  })}
+
 
 function selectTopic() {
  if (randomTopic === 'HTML') {
